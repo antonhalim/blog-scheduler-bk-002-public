7a81ab2a7a5e736aa91dadcaa886fd40498f6dc4
Katie Hoffman <ktahoffman@gmail.com>
2014-10-08 17:47:00 -0400

    stash issues

--- blog_post_scheduler.rb
+++ blog_post_scheduler.rb
@@ -1,5 +1,3 @@
 def create_groups
   # code goes here
 end
-
-[["Francine D. Blau", "János Kornai", "Arthur Goldhammer"], ["Ungku Abdul Aziz", "Raúl Prebisch", "Edith Abbott"], ["Thomas Piketty", "Zhou Xiaochuan", "Francine D. Blau"], ["János Kornai", "Arthur Goldhammer", "Ungku Abdul Aziz"], ["Raúl Prebisch", "Edith Abbott", "Thomas Piketty"]]
