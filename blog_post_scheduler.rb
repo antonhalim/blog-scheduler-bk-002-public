c2629e889a207ad48aa3248fcdd0eef8ef03377c
Katie Hoffman <ktahoffman@gmail.com>
2014-10-08 17:43:36 -0400

    update to include array

--- blog_post_scheduler.rb
+++ blog_post_scheduler.rb
@@ -1,3 +1,5 @@
 def create_groups
   # code goes here
 end
+
+[["Francine D. Blau", "János Kornai", "Arthur Goldhammer"], ["Ungku Abdul Aziz", "Raúl Prebisch", "Edith Abbott"], ["Thomas Piketty", "Zhou Xiaochuan", "Francine D. Blau"], ["János Kornai", "Arthur Goldhammer", "Ungku Abdul Aziz"], ["Raúl Prebisch", "Edith Abbott", "Thomas Piketty"]]
